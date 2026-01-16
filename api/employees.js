41053f88b2723ebe4aeadaf5bde2872eb10377d6
JT <jstorbit@gmail.com>
2026-01-15 22:32:00 -0700

    Add nodemon for more dynamic server with changes

--- api/employees.js
+++ api/employees.js
@@ -3,18 +3,18 @@ import employees from "#db/employees";
 
 const employeeRouter = express.Router();
 
-employeeRouter.get("/employees", (req, res) => {
+employeeRouter.get("/", (req, res) => {
   res.send(employees);
 });
 
 // Note: this middleware has to come first! Otherwise, Express will treat
 // "random" as the argument to the `id` parameter of /employees/:id.
-employeeRouter.get("/employees/random", (req, res) => {
+employeeRouter.get("/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
   res.send(employees[randomIndex]);
 });
 
-employeeRouter.get("/employees/:id", (req, res) => {
+employeeRouter.get("/:id", (req, res) => {
   const { id } = req.params;
 
   // req.params are always strings, so we need to convert `id` into a number
